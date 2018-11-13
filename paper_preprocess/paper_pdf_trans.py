101bfd864d91f8bb8a8980c6a176caae7ad5faff
adkmr <tty8128@bu.edu>
2018-11-13 07:32:31 -0500

    small tweaks on pdf paper trans

--- paper_preprocess/paper_pdf_trans.py
+++ paper_preprocess/paper_pdf_trans.py
@@ -9,15 +9,15 @@ from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.pdfdevice import PDFDevice
 from pdfminer.layout import LAParams
-from pdfminer.converter import HTMLConverter
+from pdfminer.converter import HTMLConverter, TextConverter
 
-
+# options for pdfparser
 pdf_directory = 'eg_paper.pdf'
 html_directory = 'eg_paper.html'
+txt_directory = 'eg_paper.txt'
 pagenos = set()
 debug = 0
 maxpages = 0
-# output option
 imagewriter = None
 rotation = 0
 stripcontrol = False
@@ -30,26 +30,24 @@ showpageno = True
 laparams = LAParams()
 
 
-def pdf_parse(pdf_directory, password=None):
+def pdf2txt(pdf_directory, txt_directory, password=None):
+    rsrcmgr = PDFResourceManager(caching=caching)
+    outfp = open(txt_directory, 'wb')
+    # device = TextConverter(rsrcmgr, outfp, codec=codec, laparams=laparams,
+    #                        imagewriter=imagewriter)
+    device = TextConverter(rsrcmgr, outfp, codec=codec, laparams=laparams,
+                           imagewriter=imagewriter)
     # Open a PDF file.
     fp = open(pdf_directory, 'rb')
-    # Create a PDF parser object associated with the file object.
-    parser = PDFParser(fp)
-    # Create a PDF document object that stores the document structure.
-    # Supply the password for initialization.
-    document = PDFDocument(parser, password)
-    # Check if the document allows text extraction. If not, abort.
-    if not document.is_extractable:
-        raise PDFTextExtractionNotAllowed
-    # Create a PDF resource manager object that stores shared resources.
-    rsrcmgr = PDFResourceManager()
-    # Create a PDF device object.
-    device = PDFDevice(rsrcmgr)
-    # Create a PDF interpreter object.
     interpreter = PDFPageInterpreter(rsrcmgr, device)
-    # Process each page contained in the document.
-    for page in PDFPage.create_pages(document):
+    for page in PDFPage.get_pages(fp, pagenos,
+                                  maxpages=maxpages, password=password,
+                                  caching=caching, check_extractable=True):
+        page.rotate = (page.rotate+rotation) % 360
         interpreter.process_page(page)
+    fp.close()
+    device.close()
+    outfp.close()
 
 
 def pdf2html(pdf_directory, html_directory, password=None):
@@ -76,6 +74,7 @@ def pdf2html(pdf_directory, html_directory, password=None):
 def main():
     # pdf_parse(pdf_directory)
     pdf2html(pdf_directory, html_directory)
+    pdf2txt(pdf_directory, txt_directory)
 
 
 if __name__ == "__main__":
