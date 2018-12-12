1707b0a07d66dcc80f9e7f3d6a0278b8b3b8f682
tty8128 <tty8128@outlook.com>
2018-12-11 21:43:01 -0500

    ui tweak and fix icon

--- Website/auto_summary/forms.py
+++ Website/auto_summary/forms.py
@@ -7,7 +7,7 @@ class NameForm(forms.Form):
 
 class ShortInputForm(forms.Form):
     short_input = forms.CharField(widget=forms.Textarea(
-        attrs={'cols': '30', 'rows': '8',
+        attrs={'cols': '30', 'rows': '5',
                'placeholder': 'If you cannot get satisfying result, maybe you are not entering meaningful sentences.',
                'required': True}), min_length=400, max_length=3000)
 
@@ -15,4 +15,4 @@ class ShortInputForm(forms.Form):
 class ContactForm(forms.Form):
     user_name = forms.CharField(required=True, max_length=50)
     user_email = forms.EmailField(required=True)
-    user_message = forms.CharField(widget=forms.Textarea(attrs={'rows': '6'}), required=True )
+    user_message = forms.CharField(widget=forms.Textarea(attrs={'rows': '6'}), required=True)
